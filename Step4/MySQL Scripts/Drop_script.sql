e08bdefa1592c83c87eac6db4a76d11d629b2315
yennyhou <92691111+yennyhou@users.noreply.github.com>
2022-07-02 19:25:20 -0700

    added MySQL in Step4 folder

---
+++ Step4/MySQL Scripts/Drop_script.sql
@@ -0,0 +1,11 @@
+-- MySQL Version of scripts for dropping tables
+USE Project_Gr8_Student_Club;
+
+DROP TABLE Alumnus_WorkHistory;
+DROP TABLE Member_WorksOn_Project;
+DROP TABLE Member_Joins_Group;
+DROP TABLE Project;
+DROP TABLE Event;
+DROP TABLE Club_Group;
+DROP TABLE Club;
+DROP TABLE Member;
