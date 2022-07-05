39d587ac8dc975b908442e7ef43f3216030a95dc
Hector Onato <honato@Hectors-MacBook-Pro.local>
2022-07-04 21:33:28 -0700

    added ms sql ddl

---
+++ MSSQL/DropTableProj.sql
@@ -0,0 +1,9 @@
+USE ProjectSQL;
+DROP TABLE Alumnus_WorkHistory;
+DROP TABLE Member_WorksOn_Project;
+DROP TABLE Member_Joins_Group;
+DROP TABLE Project;
+DROP TABLE Event;
+DROP TABLE Club_Group;
+DROP TABLE Club;
+DROP TABLE Member;
---
+++ MSSQL/InsertValuesProj.sql
@@ -0,0 +1,5 @@
+USE ProjectSQL;
+INSERT INTO Member VALUES (1234, 'hi', 'world', 'SS');
+
+SELECT *
+FROM Member
